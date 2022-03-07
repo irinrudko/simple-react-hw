d86914a2246c0fda72d26d29635ee4e73bd6acd6
Iryna Rudzko <irinrudko@gmail.com>
2022-03-07 18:04:24 +0300

    feat: add styles to header

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import style from './Header.module.css'
 import { NavLink } from 'react-router-dom'
 import { PATH } from './Routers'
 
 function Header() {
     return (
         <div>
-            <NavLink to={PATH.PRE_JUNIOR}> PreJunior </NavLink>
-            <NavLink to={PATH.JUNIOR}> Junior </NavLink>
-            <NavLink to={PATH.JUNIOR_PLUS}> JuniorPlus </NavLink>
-
-        </div >
+            <div className={style.header}>
+                <NavLink to={PATH.PRE_JUNIOR} className={`${style.link} ${style.yellow}`} > PreJunior </NavLink>
+                <NavLink to={PATH.JUNIOR} className={`${style.link} ${style.blue}`}> Junior </NavLink>
+                <NavLink to={PATH.JUNIOR_PLUS} className={`${style.link} ${style.pink}`}> JuniorPlus </NavLink>
+            </div >
+        </div>
     )
 }
 
