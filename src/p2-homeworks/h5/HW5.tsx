1f502ef97de0b349034cb16e6982afd021df4155
Iryna Rudzko <irinrudko@gmail.com>
2022-03-07 16:55:55 +0300

    feat: add all imports

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import { HashRouter } from 'react-router-dom'
 import Header from './Header'
-import Routes from './Routes'
+import Routers from './Routers'
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+            <HashRouter>
 
-            <Header />
+                <Header />
 
-            <Routes />
+                <Routers />
 
-            {/*</HashRouter>*/}
+            </HashRouter>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5/Routers.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom'
+import Error404 from './pages/Error404'
+import PreJunior from './pages/PreJunior'
+
+export const PATH = {
+    PRE_JUNIOR: 'pre-junior',
+    // add paths
+}
+
+function Routers() {
+    return (
+        <div>
+            {/*Switch выбирает первый подходящий роут*/}
+            <BrowserRouter>
+                <Routes>
+
+                    {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
+                    {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
+                    <Route path={'/'} element={() => <Navigate to={PATH.PRE_JUNIOR} />} />
+
+                    <Route path={`${PATH.PRE_JUNIOR}`} element={() => <PreJunior />} />
+                    {/* // add routes  джун, джун+ */}
+
+
+
+                    {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
+                    <Route element={() => <Error404 />} />
+
+                </Routes>
+            </BrowserRouter>z
+        </div>
+    )
+}
+
+export default Routers
