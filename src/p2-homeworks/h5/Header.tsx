9d27e30401a7a57318fbbc833556fc2d02b6933b
Iryna Rudzko <irinrudko@gmail.com>
2022-03-07 17:39:58 +0300

    feat: add navlinks to Header.tsx

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { PATH } from './Routers'
 
 function Header() {
     return (
         <div>
-            // add NavLinks
+            <NavLink to={PATH.PRE_JUNIOR}> PreJunior </NavLink>
+            <NavLink to={PATH.JUNIOR}> Junior </NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}> JuniorPlus </NavLink>
 
-        </div>
+        </div >
     )
 }
 
